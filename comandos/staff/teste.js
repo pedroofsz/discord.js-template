d3aafa7992905fa8217deca2577fad38ab776969
Pedro "Loger <87005561+pedroofsz@users.noreply.github.com>
2021-07-06 16:17:54 -0300

    Add files via upload

---
+++ comandos/staff/teste.js
@@ -0,0 +1,11 @@
+module.exports = {
+    nome: "teste",
+    aliases: ["teste"],
+    categoria: "staff",
+    desc: "Comando de testes do adm.",
+    uso: "",
+    run: async (bot, message, args) => {
+
+
+    }
+}
